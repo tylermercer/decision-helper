1e4b1374bb117afc984685fc0393e42ee43664f7
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-18 00:58:41 -0600

    Start on PairListPresenter

--- src/components/PairListPresenter.svelte
+++ src/components/PairListPresenter.svelte
@@ -1,3 +1,26 @@
 <script>
-  import PairPresenter from './PairPresenter.svelte'
+  import PairPresenter from './PairPresenter.svelte';
+  import { createEventDispatcher } from 'svelte';
+  
+  const dispatch = createEventDispatcher();
+
+  export let items = [];
+
+  let scoredItems = items.map(e => ({ ...e, score: 0 }));
+
+  const submit = () => {
+    let normalizedScores = scoredItems.map(e => ({ 
+      ...e, 
+      score: e.score/scoredItems.length
+    }));
+    dispatch('done', normalizedScores);
+  }
+
+  console.log(scoredItems);
 </script>
+
+<div>
+  <p>Pair List Presenter</p>
+  <PairPresenter></PairPresenter>
+  <button on:click={submit}>Submit</button>
+</div>
