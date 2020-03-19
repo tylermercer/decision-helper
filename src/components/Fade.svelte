c0a08bbda277fb6ff173659e97e586e9bcf09376
Tyler Mercer <tyleralanmercer@gmail.com>
2020-03-19 15:12:54 -0600

    Get closer to having a proper fade transitioning

--- src/components/Fade.svelte
+++ src/components/Fade.svelte
@@ -1,10 +1,29 @@
 <script>
-  import getFadeInOut from '../utils/fadeInOut.js';
   import {quadIn, quadOut} from 'svelte/easing';
   export let duration = 250;
-  const [fadeIn, fadeOut] = getFadeInOut(duration, quadIn, quadOut);
+  export let easeIn = quadIn;
+  export let easeOut = quadOut;
+  
+  const fadeIn = function(node) {
+    const o = +getComputedStyle(node).opacity;
+    node.hidden = true;
+    setTimeout(() => node.hidden = false, duration/2);
+    return {
+      delay: duration/2,
+      duration:duration,
+      css: t => `opacity: ${easeIn(t) * o};`
+    };
+  };
+
+  const fadeOut = function(node) {
+  const o = +getComputedStyle(node).opacity;
+    return {
+      duration:duration/2,
+      css: t => `opacity: ${easeOut(t) *o}`
+    };
+  };
 </script>
 
 <div in:fadeIn|local out:fadeOut|local>
-	<slot></slot>
+  <slot></slot>
 </div>
